d771bde3b5c255dc7fca85269e8c345de7ac2ce1
Barish Barla <barish.barla10@gmail.com>
2020-09-08 04:01:14 +0530

    Pt 5: Animation player

---
+++ Player/player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+const ACCELERATION = 480
+const MAX_SPEED = 120
+const FRICTION = 600
+var velocity = Vector2.ZERO
+
+onready var animation_player = $AnimationPlayer
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animation_player.play("RunRight")
+		else:
+			animation_player.play("RunLeft")
+
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		animation_player.play("IdleRight")
+	velocity = move_and_slide(velocity)
